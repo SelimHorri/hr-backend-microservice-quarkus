29f3e9fb4abb1e3845f441df8719c9b808be9889
SelimHorri <horri.slim@gmail.com>
2023-04-20 02:19:44 +0100

    init

---
+++ department-service/src/main/java/com/selimhorri/app/MyLivenessCheck.java
@@ -0,0 +1,19 @@
+package com.selimhorri.app;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class MyLivenessCheck implements HealthCheck {
+	
+	@Override
+	public HealthCheckResponse call() {
+		return HealthCheckResponse.up("alive");
+	}
+	
+}
+
+
+
+
